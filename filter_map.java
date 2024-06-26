4351e97fb6ea6b8d5fbd058ee1bacc2f13a477c5
Anurudha_Singh <singhanurudh570@gmail.com>
2024-06-26 11:47:31 +0530

    Implemented custom Predicate implementation for the filter function

--- filter_map.java
+++ filter_map.java
@@ -1,15 +1,22 @@
 import java.util.*;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 // import java.util.stream.Stream;
 
 class FilterMapExample {
+    // static Predicate <Integer> p=new Predicate<Integer>(){ //Predicate is a functional interface hence anonymous class knows which method to implement and we ca use lambda function
+    //     public boolean test(Integer t) {
+    //         return t%2==0;
+    //     };
+    // };
+    static Predicate <Integer> p=(t)-> t%2==0;
 
     static void showFilterMapWorking(){
         List<String> numbers = Arrays.asList("1", "2", "3", "4", "5", "6");
         System.out.println("original list: " + numbers);
         List<Integer> even = numbers.stream()
                                     .map(s -> Integer.valueOf(s))
-                                    .filter(number -> number % 2 == 0)
+                                    .filter(p)
                                     .collect(Collectors.toList());
       
         System.out.println("processed list, only even numbers: " + even);
