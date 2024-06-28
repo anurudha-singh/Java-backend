99842ab1351aac29cb85a362a5dba70206a5695b
Anurudha_Singh <singhanurudh570@gmail.com>
2024-06-28 07:48:06 +0530

    Hibernate configuration is under progress

--- hibernate tutorial/hibernate_tutorial/src/main/java/com/example/hibernate_tutorial/App.java
+++ hibernate tutorial/hibernate_tutorial/src/main/java/com/example/hibernate_tutorial/App.java
@@ -1,5 +1,11 @@
 package com.example.hibernate_tutorial;
 
+import org.hibernate.cfg.Configuration;
+
+import org.hibernate.Session;
+
+import org.hibernate.SessionFactory;
+
 /**
  * Hello world!
  *
@@ -9,5 +15,17 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        Alien alien=new Alien();
+        alien.setAid(1);
+        alien.setColor("Blue");
+        alien.setName("Jadu");
+
+        Configuration con=new Configuration().configure("/resources/hibernate.cfg.xml").
+        addAnnotatedClass(Alien.class);
+        SessionFactory sf =con.buildSessionFactory();
+        Session session=sf.openSession();
+
+        // session.save(alien);
+        
     }
 }
